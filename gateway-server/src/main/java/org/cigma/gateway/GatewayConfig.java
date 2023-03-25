8bbea1f43731993fbd9a09eb4d824e937a784573
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-03-25 16:31:40 +0000

    add gateway server with dynamic routes configuration in java file

--- gateway-server/src/main/java/org/cigma/gateway/GatewayConfig.java
+++ gateway-server/src/main/java/org/cigma/gateway/GatewayConfig.java
@@ -1,5 +1,8 @@
 package org.cigma.gateway;
 
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +11,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    @Bean
+    //@Bean
     RouteLocator locator (RouteLocatorBuilder builder){
         return builder.routes()
-                .route(r-> r.path("/api/v1/customer/**").uri("http://localhost:8090/"))
-                .route(r-> r.path("/api/v1/fraud-check/**").uri("http://localhost:8081/"))
+                .route(r-> r.path("/api/v1/customer/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r-> r.path("/api/v1/fraud-check/**").uri("lb://FRAUD-SERVICE"))
                 .build();
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
     }
 
 }
